a02e5162b315a47c64b4954834aca49ddb5c5ccf
aabreu <augusto.aabreu@gmail.com>
2015-09-01 23:12:35 -0300

    Primeiro commit com rebase

--- TesteGit.java
+++ TesteGit.java
@@ -2,5 +2,6 @@ public class TesteGit {
 public static void main (String[] args) {
 System.out.println("Teste com o GIT");
 System.out.println("Outra linha inserida");
+System.out.println("Teste feito com Rebase");
 }
 }

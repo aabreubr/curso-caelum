7df72036a7ecf76602e5bfed68ab1c8f87a66d90
aabreu <augusto.aabreu@gmail.com>
2015-09-01 23:15:01 -0300

    Outro historico inserido no commit

--- TesteGit.java
+++ TesteGit.java
@@ -3,5 +3,6 @@ public static void main (String[] args) {
 System.out.println("Teste com o GIT");
 System.out.println("Outra linha inserida");
 System.out.println("Teste feito com Rebase");
+System.out.println("Outra linha inserida para o rebase");
 }
 }

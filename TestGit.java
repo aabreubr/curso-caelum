144e100fea313d6d963056275681dff8fc1305a0
aabreu <augusto.aabreu@gmail.com>
2015-09-19 12:35:48 -0300

    Ajuste da identação de código

--- TestGit.java
+++ TestGit.java
@@ -1,6 +1,7 @@
 public class TestGit {
-public static void main (String[] args){
-System.out.println("Teste do GIT");
-System.out.println("Outra linha inserida");
-}
+	public static void main (String[] args){
+		System.out.println("Teste do GIT");
+		System.out.println("\tOutra linha inserida com identacao");
+
+	}
 }
